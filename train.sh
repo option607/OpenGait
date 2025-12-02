d741cc73441b1710337de82594bf132e0524410f
qinxiaohuan <757145890@qq.com>
2025-12-02 15:41:46 +0800

    chore(config): update dataset paths and training settings for Gait3D
    - Set dataset_root to specific path in DeepGaitV2 and Skeletongait configs
    - Increase num_workers from 1 to 4 for data loading efficiency
    - Change Backbone in_channels from 2 to 1 in Skeletongait model configuration
    - Add Gait3D training command with 4 GPUs in train.sh script

--- train.sh
+++ train.sh
@@ -30,3 +30,6 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 o
 
 # GaitGL
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 opengait/main.py --cfgs ./configs/gaitgl/gaitgl_OUMVLP.yaml --phase train
+
+# # **************** For Gait3D ****************
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 opengait/main.py --cfgs /media/data/qinxiaohuan/OpenGait/configs/skeletongait/skeletongait_Gait3D.yaml --phase train
